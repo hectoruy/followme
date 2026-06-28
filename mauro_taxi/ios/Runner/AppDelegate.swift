a0b92664607e8463fff5fa1b6d08e725a39dce05
hectoruy <freire.hector@gmail.com>
2026-06-28 16:15:57 -0400

    Fix iOS build: use standard FlutterAppDelegate, downgrade livekit_client to 2.6.4

--- mauro_taxi/ios/Runner/AppDelegate.swift
+++ mauro_taxi/ios/Runner/AppDelegate.swift
@@ -3,19 +3,13 @@ import UIKit
 import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Required by flutter_local_notifications so notifications are handled
-    // while the app is in the foreground (FlutterAppDelegate conforms to
-    // UNUserNotificationCenterDelegate).
     UNUserNotificationCenter.current().delegate = self
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
